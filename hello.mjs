d8c869322a406769a7cd4de6e9a62fc7d2efda6f
Noor Latif <noor.crystal@gmail.com>
2022-01-18 08:29:11 +0100

    http: use setHeader instead of writeHead

--- hello.mjs
+++ hello.mjs
@@ -8,7 +8,7 @@ const port = 8000;
 const server = createServer((req, res) => {
 
    // Set the response HTTP header with HTTP status and Content type
-   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.setHeader('Content-Type', 'text/html');
 
    // Send the response body "Hello World"
    res.end('Hello World\n');
